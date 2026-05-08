62b6bc6c2df85c564f65e9b96baea27565042b59
ksyyta228 <ksenaazon@gmail.com>
2026-05-08 12:47:21 +0300

    PZ_13 комит последний

--- PZ_13/13pz.py
+++ PZ_13/13pz.py
@@ -8,19 +8,25 @@ import re
 
 with open('hotline.txt', 'r', encoding='utf-8') as f:
     text = f.read()
-new_text, count = re.subn(
-    r'Горячая линия',
-    r'Горячая линия Министерства образования Ростовской области',
-    text
-)
+new_text, s = re.subn(r'Горячая линия', r'Горячая линия Министерства образования Ростовской области', text)
 with open('hotline.txt', 'w', encoding='utf-8') as f:
     f.write(new_text)
-print(f'Добавлений: {count}')
+print(f'Добавлений: {s}')
 phones = re.findall(r'\d{10,12}', new_text)
-end_03 = sum(1 for p in phones if p.endswith('03'))
-end_50 = sum(1 for p in phones if p.endswith('50'))
-print(f'На 03: {end_03}')
-print(f'На 50: {end_50}')
-ege_phones = re.findall(r'(\d{10,12})(?=.*(?:ЕГЭ|ГИА))|(?:ЕГЭ|ГИА).*?(\d{10,12})', new_text, re.IGNORECASE)
-ege_phones = [p for match in ege_phones for p in match if p]
+
+count = 0
+for p in phones:
+    if p.endswith('03'):
+        count += 1
+
+count2 = 0
+for p in phones:
+    if p.endswith('50'):
+        count2 += 1
+
+print(f'На 03: {count}')
+print(f'На 50: {count2}')
+
+ege_phones = re.findall(r'(\d{10,12})(?=.*(?:ЕГЭ|ГИА))|(?:ЕГЭ|ГИА).*?(\d{10,12})', new_text)
+ege_phones = sum([list(filter(None, match)) for match in ege_phones], [])
 print(f'Телефоны ЕГЭ/ГИА: {ege_phones}')
