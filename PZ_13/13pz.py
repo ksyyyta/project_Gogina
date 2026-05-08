60fd27e87eb7bae53ba49c16b15438ebe98df937
ksyyta228 <ksenaazon@gmail.com>
2026-05-08 07:55:57 +0300

    PZ_13 КОМИТ последний

--- PZ_13/13pz.py
+++ PZ_13/13pz.py
@@ -3,7 +3,6 @@
 фразу «Министерства образования Ростовской области», посчитать количество
 произведённых добавлений. Сколько номеров телефонов заканчивается на «03»,
 «50». Вывести номера телефонов горячих линий, связанных с ЕГЭ/ГИА.
-
 """
 import re
 
@@ -14,20 +13,14 @@ new_text, count = re.subn(
     r'Горячая линия Министерства образования Ростовской области',
     text
 )
-
 with open('hotline.txt', 'w', encoding='utf-8') as f:
     f.write(new_text)
-
 print(f'Добавлений: {count}')
 phones = re.findall(r'\d{10,12}', new_text)
-
 end_03 = sum(1 for p in phones if p.endswith('03'))
 end_50 = sum(1 for p in phones if p.endswith('50'))
-
 print(f'На 03: {end_03}')
 print(f'На 50: {end_50}')
-
 ege_phones = re.findall(r'(\d{10,12})(?=.*(?:ЕГЭ|ГИА))|(?:ЕГЭ|ГИА).*?(\d{10,12})', new_text, re.IGNORECASE)
 ege_phones = [p for match in ege_phones for p in match if p]
-
 print(f'Телефоны ЕГЭ/ГИА: {ege_phones}')
